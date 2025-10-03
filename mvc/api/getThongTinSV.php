40e3faa96c2277f975b54e36f1e6940f2eac8a5e
NgoBao-14 <ngobao3861@gmail.com>
2025-10-03 22:09:52 +0700

    ngobao

---
+++ mvc/api/getThongTinSV.php
@@ -0,0 +1,8 @@
+<?php
+    require_once("../Bridge.php");
+include("../class/classketnoi.php");
+include("../private/AES.php");
+$r = new giaimaAES();
+	$p = new csdl();
+	$p->GetThongTinSV();
+?>
--- mvc/core/DB.php
+++ mvc/core/DB.php
@@ -7,10 +7,21 @@ class DB{
     protected $password = "123456";
     protected $dbname = "thongtinmay";
 
+    public $api = "http://localhost:8080/CongNgheMoi/mvc/api/";
+
     function __construct(){
         $this->connect = mysqli_connect($this->servername, $this->username, $this->password);
         mysqli_select_db($this->connect, $this->dbname);
         mysqli_query($this->connect, "SET NAMES 'utf8'");
+    }
+    public function docjson($url)
+    {
+        $client=curl_init($url);
+		curl_setopt($client,CURLOPT_RETURNTRANSFER,1);
+		$response=curl_exec($client);
+		$results=json_decode($response);
+		return $results; 
+
     }
 }
 
